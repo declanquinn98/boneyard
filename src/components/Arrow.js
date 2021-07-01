aab413f46d9a178c04f41ea6deb51e1750cae8ef
Declan <dquinn133131@gmail.com>
2021-07-01 18:12:17 +1000

    insta test

--- src/components/Arrow.js
+++ src/components/Arrow.js
@@ -35,8 +35,10 @@ export const Arrow = (props) => {
     return (
         <div className="cDiv">
             <svg
-                className={props.up ? 'cSvgUp' : 'cSvgDown'}
+                width="100%" 
                 viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+                className={props.up ? 'cSvgUp' : 'cSvgDown'}
             >
                 <filter
                     id="shadow"
--- src/components/IndexSections/Contact.js
+++ src/components/IndexSections/Contact.js
@@ -3,6 +3,10 @@ import { useRef, useEffect, useState, Component } from "react";
 import { config } from 'react-spring';
 import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax';
 import GoogleMap from 'google-map-react';
+import { useStaticQuery, graphql } from "gatsby"
+
+import InstagramFeed from 'react-ig-feed'
+import 'react-ig-feed/dist/index.css'
 
 import fb from "../../assets/images/facebook.png"
 import insta from "../../assets/images/insta.png"
@@ -10,11 +14,62 @@ import insta from "../../assets/images/insta.png"
 import mapStyle from "../../styles/map.js"
 import styles from '../../styles/marker.css';
 
+
 const offBlack = "#1f1d1e";
 const offWhite = "#f4f4f9";
 
+const instajson = {
+    "data": [
+        {
+            "media_type": "IMAGE",
+            "permalink": "https://www.instagram.com/p/BiiNFxVh_OW/",
+            "media_url": "https://scontent-syd2-1.cdninstagram.com/v/t51.2885-15/31686867_367427683744705_6208149349742936064_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=8ae9d6&_nc_ohc=fFpDGSF9jAoAX-g8qLu&_nc_ht=scontent-syd2-1.cdninstagram.com&oh=e01150d3e9e4484745d62a0380fce915&oe=60E31688",
+            "id": "17930853136099183"
+        },
+        {
+            "media_type": "IMAGE",
+            "permalink": "https://www.instagram.com/p/BiiNER0BzuG/",
+            "media_url": "https://scontent-syd2-1.cdninstagram.com/v/t51.2885-15/31975899_579558162426588_3435362293586591744_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=8ae9d6&_nc_ohc=ieHqZ1GVzQ4AX9Ob1vt&_nc_ht=scontent-syd2-1.cdninstagram.com&oh=06dd2288baf4a778c2bba648e8f5b76b&oe=60E2307F",
+            "id": "17948267479034212"
+        },
+        {
+            "media_type": "IMAGE",
+            "permalink": "https://www.instagram.com/p/BiiNCeBBVhq/",
+            "media_url": "https://scontent-syd2-1.cdninstagram.com/v/t51.2885-15/31412207_203581533583116_4920453601043677184_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=8ae9d6&_nc_ohc=PcbpmQsStcsAX96g4-l&_nc_ht=scontent-syd2-1.cdninstagram.com&oh=035ee1bbcb94c7146ac785e5b5541cf0&oe=60E24F22",
+            "id": "17844666418263549"
+        }
+    ],
+    "paging": {
+        "cursors": {
+            "before": "QVFIUldjUjlhY1NvWGpkcWU2Q3IzXzh1bVcxMDBNRUpCTVdvendNb2wyY0ptcjhHUU1zbjF0Y0kwY1RVaWhnRV9JRjdWMThqYlFmWjVHSi14MGs4MV9KaHdR",
+            "after": "QVFIUjUtd0ZA0RHp3X0Q1ZAE1sMVEtOTUxRUhtWjlLMmVHbl9sM0VlTnBhSF9UMWpKQlNBYmFvUTlhZAjFwRzQzY1FfUi1uZA2ttSFVCcmduUDBKalJ2TlN1Xzh3"
+        }
+    }
+};
+
+
 export const Contact = () => {
 
+    const [instaArray, setInstaArray] = useState([]);
+
+    useEffect(() => {
+        FetchInstaFeed();
+    }, [])
+
+    const FetchInstaFeed = async () => {
+
+        const url = process.env.INSTAGRAM_URL + "" + process.env.INSTAGRAM_TOKEN
+
+        setInstaArray(instajson);
+        /*
+        await fetch(url)
+            .then(response => response.json())
+            .then(json => {
+                //Doesnt work on localhost??
+                //setInstaArray(json.data);
+            });
+            */
+    }
 
     return (
         <div
@@ -119,10 +174,9 @@ export const Contact = () => {
                                 width: "100%",
                                 height: "25%",
                                 marginTop: "auto",
-                                backgroundColor: "red"
                             }}
                         >
-
+                            <InstagramFeed token={process.env.INSTAGRAM_TOKEN} counter="6" />
                         </div>
                     </div>
 
